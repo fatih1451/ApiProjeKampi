b400c76e15d934a20d0019ed444773540fc40cf2
Fatih SARIYILDIZ <fatih.sariyildiz@pusulayazilim.com.tr>
2025-12-18 23:28:35 +0300

    41. Ders

---
+++ ApiProjeKampi.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,85 @@
+﻿using System.Text;
+using ApiProjeKampi.WebUI.Dtos.ProductDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> ProductList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7124/api/Products");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createProductDto);
+
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            var responseMessage = await client.PostAsync("https://localhost:7124/api/Products", content);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ProductList");
+            }
+
+            return View();
+        }
+
+
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            await client.DeleteAsync($"https://localhost:7124/api/Products?id={id}");
+            return RedirectToAction("ProductList");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateProduct(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7124/api/Products/GetProducts?id=" + id);
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+
+            var value = JsonConvert.DeserializeObject<GetProductByIdDto>(jsonData);
+            return View(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateProduct(UpdateProductDto updateProductDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateProductDto);
+
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            await client.PutAsync("https://localhost:7124/api/Products", content);
+            return RedirectToAction("ProductList");
+        }
+    }
+}
